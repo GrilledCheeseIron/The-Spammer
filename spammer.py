92a78410cf4c66f8b80ca1cd55c990437e98e63c
numbers1234 <118110925+numbers1234@users.noreply.github.com>
2023-02-22 13:25:18 +0100

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -10,6 +10,7 @@ print("1: info/about")
 print("2: tutorial/how to use")
 print("3: whatsapp/instagram")
 print("4: tiktok")
+print("5: youtube")
 
 
 optionsrun = input("on what social media app would you want to run a script or what would you like to see?: ")
@@ -21,10 +22,10 @@ if optionsrun == "1":
     print("so i made this project the spammer")
     print("you can alse see it as a collection of spambots/spammers.")
     print("ignore the error that comes after this description the script works and i dont know how to get rid of it")
-    print("note: this script only works with 1920 x 1080 displa in landscape mode")
+    print("note: the tiktok and youtube script only works with 1920 x 1080 displa in landscape mode")
 
 if optionsrun == "2":
-    print("if you want to stop the programm simply wait for the programm to stop spamming/ or close the terminal")
+    print("if you want to stop the programm simply wait for the programm to stop spamming or close the terminal")
     print("if you are going to us the whatsapp/instagram spambot")
     print("1, go to whatsapp on you're browser")
     print("2, run the script")
@@ -40,6 +41,7 @@ if optionsrun == "3":
     print ("scripts for whatsapp or instagram:")
     print("1, rickroll spam script (spams the lyrics of never gonna give you up) ")
     print("2, normal spam (spams a custom amount of messages and a custom message)")
+    print("3, the entire bee movie script")
     optionsrunwhatsapp = input("what script would you like to run? ")
 
     if optionsrunwhatsapp == "1":
@@ -63,6 +65,13 @@ if optionsrun == "3":
         pyautogui.press("enter")
         i = i+1
 
+    if optionsrunwhatsapp == "3":
+        print("go to whatsapp or instagram and wait 10 seconds")
+        time.sleep(10)
+        f= open("beemoviescript.txt", 'r')
+        for word in f:
+            pyautogui.typewrite(word)
+            pyautogui.press("enter")
 
 if optionsrun == "4":
     print("options:")
